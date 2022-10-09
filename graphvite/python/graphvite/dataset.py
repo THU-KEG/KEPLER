28245322fa876c0e14e61763800d4304d887c13d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 20:46:15 +0000

    Adding tarfile member sanitization to extractall()

--- graphvite/python/graphvite/dataset.py
+++ graphvite/python/graphvite/dataset.py
@@ -160,7 +160,26 @@ class Dataset(object):
             if member is None:
                 logger.info("extracting %s to %s" % (self.relpath(zip_file), self.relpath(save_file)))
                 with tarfile.open(zip_file, "r") as fin:
-                    fin.extractall(save_file)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(fin, save_file)
             else:
                 logger.info("extracting %s from %s to %s" % (member, self.relpath(zip_file), self.relpath(save_file)))
                 with tarfile.open(zip_file, "r").extractfile(member) as fin, open(save_file, "wb") as fout:
